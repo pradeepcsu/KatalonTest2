d3629041919dce225163f674373933f588263dd9
Pradeep Patapanchala <pradeepcsu@gmail.com>
2024-09-23 23:04:19 -0500

    deleted a keyword

--- Scripts/TC_API_1/Script1727045544440.groovy
+++ Scripts/TC_API_1/Script1727045544440.groovy
@@ -17,7 +17,5 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WS.comment('API Test 1')
-
 WS.sendRequest(findTestObject('ReqRes/Req1'))
 
